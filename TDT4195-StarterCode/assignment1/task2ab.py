95a24def97b7b78076477beea00886ec653a4209
r0ti <45827644+r0ti@users.noreply.github.com>
2019-10-15 13:46:22 +0200

    Add files via upload

---
+++ TDT4195-StarterCode/assignment1/task2ab.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import os
+
+image_output_dir = "image_processed"
+os.makedirs(image_output_dir, exist_ok=True)
+
+
+def save_im(imname, im, cmap=None):
+    impath = os.path.join(image_output_dir, imname)
+    plt.imsave(impath, im, cmap=cmap)
+
+
+def greyscale(im):
+    """ Converts an RGB image to greyscale
+    
+    Args:
+        im ([type]): [np.array of shape [H, W, 3]]
+    
+    Returns:
+        im ([type]): [np.array of shape [H, W]]
+    """
+    # YOUR CODE HERE
+
+    r, g, b = im[:, :, 0], im[:, :, 1], im[:, :, 2]
+
+    im = r*0.212 + g*0.7152 + b*0.0722
+
+    return im
+
+
+def inverse(im):
+    """ Finds the inverse of the greyscale image
+    
+    Args:
+        im ([type]): [np.array of shape [H, W]]
+    
+    Returns:
+        im ([type]): [np.array of shape [H, W]]
+    """    
+     # YOUR CODE HERE
+    return im
+
+
+if __name__ == "__main__":
+    im = plt.imread("images/lake.jpg")
+    im = greyscale(im)
+    inverse_im = inverse(im)
+    save_im("lake_greyscale.jpg", im, cmap="gray")
+    save_im("lake_inverse.jpg", inverse_im, cmap="gray")
